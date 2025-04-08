71ebe23c3604ef2674200e505b6c999ebee9e0c4
Aleks Shulman <aleksshulman@Alekss-MacBook-Pro.local>
2025-04-07 18:00:14 -0700

    Fix test_order

--- common/test/order/test_order.py
+++ common/test/order/test_order.py
@@ -15,38 +15,37 @@ from common.order.order_line import OrderLine
 from common.order.order_price import OrderPrice
 from common.order.order_price_type import OrderPriceType
 
-e: Equity = Equity("GE", "General Electric")
+e: Equity = Equity(ticker="GE", company_name="General Electric")
 
 
 def test_build_single_line_order():
-    strike: Amount = Amount(10,0, Currency.US_DOLLARS)
+    strike: Amount = Amount(whole=10,part=0, currency=Currency.US_DOLLARS)
 
     order_id: str = "123"
     order_expiry: OrderExpiry = GoodForDay()
-    call_option: Tradable = Option(e, OptionType.CALL, strike, datetime.datetime(2024, 11, 5).date(), ExerciseStyle.AMERICAN)
-    order_line = OrderLine(call_option,  Action.SELL_OPEN, 1)
-    order_price: OrderPrice = OrderPrice(OrderPriceType.NET_CREDIT, Amount(0, 14, Currency.US_DOLLARS))
+    call_option: Tradable = Option(equity=e, type=OptionType.CALL, strike=strike, expiry=datetime.datetime(2024, 11, 5).date(), style=ExerciseStyle.AMERICAN)
+    order_line = OrderLine(tradable=call_option,  action=Action.SELL_OPEN, quantity=1)
+    order_price: OrderPrice = OrderPrice(order_price_type=OrderPriceType.NET_CREDIT, price=Amount(whole=0, part=14, currency=Currency.US_DOLLARS))
 
-    single_order = Order(order_id, order_expiry, [order_line], order_price)
+    single_order = Order(expiry=order_expiry, order_lines=[order_line], order_price=order_price)
 
     assert single_order is not None
 
 
 def test_build_dual_line_order():
-    strike: Amount = Amount(10, 0, Currency.US_DOLLARS)
+    strike: Amount = Amount(whole=10, part=0, currency=Currency.US_DOLLARS)
 
-    order_id: str = "123"
     order_expiry: OrderExpiry = GoodForDay()
-    call_option: Tradable = Option(e, OptionType.CALL, strike, datetime.datetime(2024, 11, 5).date(),
-                                   ExerciseStyle.AMERICAN)
-    order_line = OrderLine(call_option, Action.SELL_OPEN, 2)
+    call_option: Tradable = Option(equity=e, type=OptionType.CALL, strike=strike, expiry=datetime.datetime(2024, 11, 5).date(),
+                                   style=ExerciseStyle.AMERICAN)
+    order_line = OrderLine(tradable=call_option, action=Action.SELL_OPEN, quantity=2)
 
-    strike2: Amount = Amount(20, 0, Currency.US_DOLLARS)
-    call_option2 = Option(e, OptionType.CALL, strike2, datetime.date(2024, 11, 5), ExerciseStyle.AMERICAN)
-    order_line2 = OrderLine(call_option2, Action.BUY_OPEN, 1)
+    strike2: Amount = Amount(whole=20, part=0, currency=Currency.US_DOLLARS)
+    call_option2 = Option(equity=e, type=OptionType.CALL, strike=strike2, expiry=datetime.date(2024, 11, 5), style=ExerciseStyle.AMERICAN)
+    order_line2 = OrderLine(tradable=call_option2, action=Action.BUY_OPEN, quantity=1)
 
-    order_price: OrderPrice = OrderPrice(OrderPriceType.NET_CREDIT, Amount(0, 14, Currency.US_DOLLARS))
+    order_price: OrderPrice = OrderPrice(order_price_type=OrderPriceType.NET_CREDIT, price=Amount(whole=0, part=14, currency=Currency.US_DOLLARS))
 
-    dual_order = Order(order_id, order_expiry, [order_line, order_line2], order_price)
+    dual_order = Order(expiry=order_expiry, order_lines=[order_line, order_line2], order_price=order_price)
 
     assert dual_order is not None
