ee20d37b1b8661b577a0d8ece53a186b32cb251b
Aleks Shulman <aleksshulman@Alekss-MacBook-Pro.local>
2025-04-07 19:10:52 -0700

    Fixed TestCalendarSpreadCell

--- trident/research/test/test_calendar_spread_cell.py
+++ trident/research/test/test_calendar_spread_cell.py
@@ -8,11 +8,11 @@ from trident.research.calendar_spread_constructor import CalendarSpreadCell
 expiry_1 = datetime.datetime(2024, 11, 18).date()
 expiry_2 = datetime.datetime(2024, 11, 22).date()
 
-option_delta_atm = (Amount(1, 28, Currency.US_DOLLARS), Amount(1, 58, Currency.US_DOLLARS))
-option_delta_otm = Amount(0, 27, Currency.US_DOLLARS), Amount(0, 47, Currency.US_DOLLARS)
+option_delta_atm = (Amount(whole=1, part=28, currency=Currency.US_DOLLARS), Amount(whole=1, part=58, currency=Currency.US_DOLLARS))
+option_delta_otm = Amount(whole=0, part=27, currency=Currency.US_DOLLARS), Amount(whole=0, part=47, currency=Currency.US_DOLLARS)
 
 class TestCalendarSpreadCell:
-    equity = Equity("SPY", "SPDR")
+    equity = Equity(ticker="SPY", company_name="SPDR")
 
     def test_division(self):
         cell = CalendarSpreadCell(expiry_1, expiry_2, option_delta_atm, option_delta_otm)
@@ -20,5 +20,4 @@ class TestCalendarSpreadCell:
 
     def test_subtraction(self):
         cell = CalendarSpreadCell(expiry_1, expiry_2, option_delta_atm, option_delta_otm)
-        assert cell.get_price_difference() == Amount(0, 10, Currency.US_DOLLARS)
-        pass
+        assert cell.get_price_difference() == Amount(whole=0, part=10, currency=Currency.US_DOLLARS)
