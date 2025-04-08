dc9f7ea10ec6e6f8f9227355b81b99e2e0edfc7c
Aleks Shulman <aleksshulman@Alekss-MacBook-Pro.local>
2025-04-08 12:25:50 -0700

    Fix test_spread class for pydantic compat

--- common/test/finance/options/test_spread.py
+++ common/test/finance/options/test_spread.py
@@ -21,14 +21,14 @@ class TestSpread:
 
     @pytest.fixture
     def sfix_covered_call(self):
-        equity = Equity("SFIX", "STITCH FIX INC COM CL A")
+        equity = Equity(ticker="SFIX", company_name="STITCH FIX INC COM CL A")
         option_expiry: datetime.date = datetime(2025, 1, 31).date()
-        tradable1: Option = Option(equity, OptionType.CALL, Amount(4, 0, Currency.US_DOLLARS), option_expiry)
+        tradable1: Option = Option(equity=equity, type=OptionType.CALL, strike=Amount(whole=4, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry)
 
         ol_1 = EquityOrderLine(equity, Action.BUY, 100)
         ol_2 = OptionOrderLine(tradable1, Action.SELL_OPEN, 1)
 
-        order_price: OrderPrice = OrderPrice(OrderPriceType.NET_CREDIT, Amount(0, 44))
+        order_price: OrderPrice = OrderPrice(order_price_type=OrderPriceType.NET_CREDIT, price=Amount(whole=0, part=44))
 
         option_order_lines: list[OrderLine] = list[OrderLine]()
 
@@ -38,15 +38,15 @@ class TestSpread:
 
     @pytest.fixture
     def sfix_call_credit_spread(self) -> Spread:
-        equity = Equity("SFIX", "STITCH FIX INC COM CL A")
+        equity = Equity(ticker="SFIX", company_name="STITCH FIX INC COM CL A")
         option_expiry: datetime.date = datetime(2025, 1, 31).date()
-        tradable1: Option = Option(equity, OptionType.CALL, Amount(4, 0, Currency.US_DOLLARS), option_expiry)
-        tradable2: Option = Option(equity, OptionType.CALL, Amount(5, 0, Currency.US_DOLLARS), option_expiry)
+        tradable1: Option = Option(equity=equity, type=OptionType.CALL, strike=Amount(whole=4, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry)
+        tradable2: Option = Option(equity=equity, type=OptionType.CALL, strike=Amount(whole=5, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry)
 
-        ol_1 = OrderLine(tradable1, Action.SELL_OPEN, 1)
-        ol_2 = OrderLine(tradable2, Action.BUY_OPEN, 1)
+        ol_1 = OrderLine(tradable=tradable1, action=Action.SELL_OPEN, quantity=1)
+        ol_2 = OrderLine(tradable=tradable2, action=Action.BUY_OPEN, quantity=1)
 
-        order_price: OrderPrice = OrderPrice(OrderPriceType.NET_CREDIT, Amount(0, 44))
+        order_price: OrderPrice = OrderPrice(order_price_type=OrderPriceType.NET_CREDIT, price=Amount(whole=0, part=44))
 
         order_lines: list[OrderLine] = list[OrderLine]()
         order_lines.append(ol_1)
@@ -56,16 +56,16 @@ class TestSpread:
 
     @pytest.fixture
     def sfix_call_debit_diagonal_spread(self) -> Spread:
-        equity = Equity("SFIX", "STITCH FIX INC COM CL A")
+        equity = Equity(ticker="SFIX", company_name="STITCH FIX INC COM CL A")
         option_expiry_1: datetime.date = datetime(2025, 1, 31).date()
         option_expiry_2: datetime.date = datetime(2025, 2, 6).date()
-        tradable1: Option = Option(equity, OptionType.CALL, Amount(4, 0, Currency.US_DOLLARS), option_expiry_1)
-        tradable2: Option = Option(equity, OptionType.CALL, Amount(5, 0, Currency.US_DOLLARS), option_expiry_2)
+        tradable1: Option = Option(equity=equity, type=OptionType.CALL, strike=Amount(whole=4, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry_1)
+        tradable2: Option = Option(equity=equity, type=OptionType.CALL, strike=Amount(whole=5, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry_2)
 
-        ol_1 = OrderLine(tradable1, Action.SELL_OPEN, 1)
-        ol_2 = OrderLine(tradable2, Action.BUY_OPEN, 1)
+        ol_1 = OrderLine(tradable=tradable1, action=Action.SELL_OPEN, quantity=1)
+        ol_2 = OrderLine(tradable=tradable2, action=Action.BUY_OPEN, quantity=1)
 
-        order_price: OrderPrice = OrderPrice(OrderPriceType.NET_DEBIT, Amount(0, 5))
+        order_price: OrderPrice = OrderPrice(order_price_type=OrderPriceType.NET_DEBIT, price=Amount(whole=0, part=5))
 
         order_lines: list[OrderLine] = list[OrderLine]()
         order_lines.append(ol_1)
@@ -75,16 +75,16 @@ class TestSpread:
 
     @pytest.fixture
     def sfix_put_credit_calendar_spread(self) -> Spread:
-        equity = Equity("SFIX", "STITCH FIX INC COM CL A")
+        equity = Equity(ticker="SFIX", company_name="STITCH FIX INC COM CL A")
         option_expiry_1: datetime.date = datetime(2025, 1, 31).date()
         option_expiry_2: datetime.date = datetime(2025, 2, 6).date()
-        tradable1: Option = Option(equity, OptionType.PUT, Amount(5, 0, Currency.US_DOLLARS), option_expiry_1)
-        tradable2: Option = Option(equity, OptionType.PUT, Amount(5, 0, Currency.US_DOLLARS), option_expiry_2)
+        tradable1: Option = Option(equity=equity, type=OptionType.PUT, strike=Amount(whole=5, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry_1)
+        tradable2: Option = Option(equity=equity, type=OptionType.PUT, strike=Amount(whole=5, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry_2)
 
-        ol_1 = OrderLine(tradable1, Action.BUY_OPEN, 1)
-        ol_2 = OrderLine(tradable2, Action.SELL_OPEN, 1)
+        ol_1 = OrderLine(tradable=tradable1, action=Action.BUY_OPEN, quantity=1)
+        ol_2 = OrderLine(tradable=tradable2, action=Action.SELL_OPEN, quantity=1)
 
-        order_price: OrderPrice = OrderPrice(OrderPriceType.NET_CREDIT, Amount(0, 5))
+        order_price: OrderPrice = OrderPrice(order_price_type=OrderPriceType.NET_CREDIT, price=Amount(whole=0, part=5))
 
         order_lines: list[OrderLine] = list[OrderLine]()
         order_lines.append(ol_1)
@@ -94,15 +94,15 @@ class TestSpread:
 
     @pytest.fixture
     def sfix_call_debit_spread(self) -> Spread:
-        equity = Equity("SFIX", "STITCH FIX INC COM CL A")
+        equity = Equity(ticker="SFIX", company_name="STITCH FIX INC COM CL A")
         option_expiry: datetime.date = datetime(2025, 1, 31).date()
-        tradable1: Option = Option(equity, OptionType.CALL, Amount(4, 0, Currency.US_DOLLARS), option_expiry)
-        tradable2: Option = Option(equity, OptionType.CALL, Amount(5, 0, Currency.US_DOLLARS), option_expiry)
+        tradable1: Option = Option(equity=equity, type=OptionType.CALL, strike=Amount(whole=4, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry)
+        tradable2: Option = Option(equity=equity, type=OptionType.CALL, strike=Amount(whole=5, part=0, currency=Currency.US_DOLLARS), expiry=option_expiry)
 
-        ol_1 = OrderLine(tradable1, Action.BUY_OPEN, 1)
-        ol_2 = OrderLine(tradable2, Action.SELL_OPEN, 1)
+        ol_1 = OrderLine(tradable=tradable1, action=Action.BUY_OPEN, quantity=1)
+        ol_2 = OrderLine(tradable=tradable2, action=Action.SELL_OPEN, quantity=1)
 
-        order_price: OrderPrice = OrderPrice(OrderPriceType.NET_DEBIT, Amount(0, 44))
+        order_price: OrderPrice = OrderPrice(order_price_type=OrderPriceType.NET_DEBIT, price=Amount(whole=0, part=44))
 
         order_lines: list[OrderLine] = list[OrderLine]()
         order_lines.append(ol_1)
@@ -111,26 +111,26 @@ class TestSpread:
         return Spread(order_price, order_lines)
 
     def test_vertical_call_credit_spread_collateral_requirement(self, sfix_call_credit_spread: Spread):
-        expected_collateral_required = Amount(100, 0) - Amount(0, 44) * DE_NORMALIZATION_CONSTANT
+        expected_collateral_required = Amount(whole=100, part=0) - Amount(whole=0, part=44) * DE_NORMALIZATION_CONSTANT
         collateral_required: Amount = sfix_call_credit_spread.get_collateral_required()
 
         assert collateral_required == expected_collateral_required
 
     def test_diagonal_call_debit_spread_collateral_requirement(self, sfix_call_debit_diagonal_spread: Spread):
-        expected_collateral_required = Amount(100, 0) + Amount(0, 5) * DE_NORMALIZATION_CONSTANT
+        expected_collateral_required = Amount(whole=100, part=0) + Amount(whole=0, part=5) * DE_NORMALIZATION_CONSTANT
         collateral_required: Amount = sfix_call_debit_diagonal_spread.get_collateral_required()
 
         assert collateral_required == expected_collateral_required
 
     def test_put_calendar_credit_spread_collateral_requirement(self, sfix_put_credit_calendar_spread: Spread):
-        expected_collateral_required = Amount(500, 0) - Amount(0, 5) * DE_NORMALIZATION_CONSTANT
+        expected_collateral_required = Amount(whole=500, part=0) - Amount(whole=0, part=5) * DE_NORMALIZATION_CONSTANT
         collateral_required: Amount = sfix_put_credit_calendar_spread.get_collateral_required()
 
         assert collateral_required == expected_collateral_required
 
 
     def test_vertical_call_debit_spread_collateral_requirement(self, sfix_call_debit_spread: Spread):
-        expected_collateral_required = Amount(0, 44) * DE_NORMALIZATION_CONSTANT
+        expected_collateral_required = Amount(whole=0, part=44) * DE_NORMALIZATION_CONSTANT
         collateral_required: Amount = sfix_call_debit_spread.get_collateral_required()
 
         assert collateral_required == expected_collateral_required
